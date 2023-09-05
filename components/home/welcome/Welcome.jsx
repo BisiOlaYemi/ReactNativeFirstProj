adbebd645aec9de44875f16c4585cc6fbe7c01ae
Olayemi <ogunrinde_olayemi@yahoo.com>
2023-09-05 14:47:28 -0700

    First ReactNative Project

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,25 @@
+import React, { useState} from 'react'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+
+}   from 'react-native'
+import { useRouter } from 'expo-router'
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.userName}>Hello Yemi</Text>
+    </View>
+  )
+}
+
+export default Welcome
